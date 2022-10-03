f6d4344711396c7071a3433c8ea210ffe2c69be9
aidan <aid.hayes01@gmail.com>
2022-10-03 12:19:14 -0400

    Add comment explaining how to interpret BLEU score results

--- test.py
+++ test.py
@@ -11,6 +11,19 @@ hypothesis = "".split()
 #Original translation (eg: A sentence in English)
 reference = "".split()
 
+'''
+REF: https://cloud.google.com/translate/automl/docs/evaluate
+< .1 - Almost useless
+[.1, .19] - Hard to get the gist
+[.2 - .29] - The gist is clear, but has significant grammatical errors
+[.3 - .4] - Understandable to good translations
+[.4 - .5] - High quality translations
+[.5 - .6] - Very high quality, adequate, and fluent translations
+> .6 - Quality often better than human
+'''
 #Comparison between sentences
 
 #We split because sentence_bleu reads an array of strings (individual words)
+
+#Display result of comparison 
+print()
