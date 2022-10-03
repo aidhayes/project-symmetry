bf2f88c3eeca55bb75f26da0e9ee929eb8f346b7
aidan <aid.hayes01@gmail.com>
2022-10-03 12:20:54 -0400

    Fix comments

--- test.py
+++ test.py
@@ -4,11 +4,11 @@ from nltk.translate.bleu_score import sentence_bleu
 # For whole paragraphs/texts
 from nltk.translate.bleu_score import corpus_bleu
 
-#REF: https://www.nltk.org/api/nltk.translate.bleu_score.html
+# REF: https://www.nltk.org/api/nltk.translate.bleu_score.html
 
-#Sentence translated from another language (eg: A sentence translated from French to English)
+# Sentence translated from another language (eg: A sentence translated from French to English)
 hypothesis = "".split()
-#Original translation (eg: A sentence in English)
+# Original translation (eg: A sentence in English)
 reference = "".split()
 
 '''
