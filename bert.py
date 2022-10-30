326971dac14ac340810d426957a43f878a295bf9
aidhayes <aid.hayes01@gmail.com>
2022-10-30 16:18:56 -0400

    Implement bert for use over whole article

--- bert.py
+++ bert.py
@@ -2,6 +2,9 @@
 #To import use the command pip install -U sentence-transformers
 #Import the for the Bert model
 from sentence_transformers import SentenceTransformer, util
+import colors
+from sentence_transformers import SentenceTransformer, util
+from nltk.tokenize import sent_tokenize
 
 #SentenceTransformer takes a type of model name that is provide on the the website https://www.sbert.net/docs/pretrained_models.html.
 #'all-MiniLM-L6-v2' is the model with the highest performace number of 69.57%,
@@ -40,3 +43,22 @@ all_sentence_combinations = sorted(all_sentence_combinations, key=lambda x: x[0]
 print("Top-5 most similar pairs:")
 for score, i, j in all_sentence_combinations[0:5]:
     print("{} \t {} \t {:.4f}".format(sentences[i], sentences[j], cos_sim[i][j]))
+
+
+def bert(par1, par2, similarity=.5):
+    list1 = sent_tokenize(par1)
+    list2 = sent_tokenize(par2)
+    dict1 = dict()
+    dict2 = dict()
+
+    cos_sim = util.cos_sim(model.encode(list1), model.encode(list2))
+
+    for i in range(len(list1)):
+        for j in range(len(list2)):
+            if cos_sim[i][j] >= similarity:
+                if list1[i] not in dict1 and list2[j] not in dict2:
+                    highlight = colors.gen_color()
+                    dict1[list1[i]] = [list2[j], highlight]
+                    dict2[list2[j]] = [list1[i], highlight]
+
+    return dict1, dict2
