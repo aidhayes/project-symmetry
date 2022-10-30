45bd41514abcdcc18c24b88fc7645ffe22a15a34
aidhayes <aid.hayes01@gmail.com>
2022-10-30 16:19:09 -0400

    Implement rcg

--- bleu_score.py
+++ bleu_score.py
@@ -5,6 +5,7 @@ from nltk.translate.bleu_score import sentence_bleu
 from nltk.translate.bleu_score import corpus_bleu
 from nltk.tokenize import sent_tokenize
 from nltk.translate.bleu_score import SmoothingFunction
+import colors
 
 
 '''
@@ -14,7 +15,7 @@ def compare(ref, hypothesis, similarity=0.1):
     # Tokenize paragraphs so they can be traversed as an array
     ref_list = sent_tokenize(ref)
     hyp_list = sent_tokenize(hypothesis)
-
+    i = 0
     ref_pair_dict = dict()
     hyp_pair_dict = dict()
     # Itteration over both paragraphs
@@ -23,7 +24,8 @@ def compare(ref, hypothesis, similarity=0.1):
             # Determine if the current sentence has a match or not
             if sentence_bleu([ref.split()], hyp.split(), smoothing_function=SmoothingFunction().method7) >= similarity:
                 # Check for duplicates
-                if ref not in ref_pair_dict:
+                if ref not in ref_pair_dict and hyp not in hyp_pair_dict:
+                    highlight = colors.gen_color()
                     '''
                     key = reference
                     value = hypothesis
@@ -31,7 +33,7 @@ def compare(ref, hypothesis, similarity=0.1):
                         key = English sentence
                         value = French sentence translated to English
                     '''
-                    ref_pair_dict[ref] = hyp
+                    ref_pair_dict[ref] = [hyp, highlight]
                     
                     '''
                     key = hypothesis
@@ -40,6 +42,8 @@ def compare(ref, hypothesis, similarity=0.1):
                         key = French sentence translated to English
                         value = English sentence
                     '''
-                    hyp_pair_dict[hyp] = ref
+                    hyp_pair_dict[hyp] = [ref, highlight]
 
+        print(hyp_pair_dict)
+        print(ref_pair_dict)
     return ref_pair_dict, hyp_pair_dict
