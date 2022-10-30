0f8c21a039a6ce18fa092c034c62b510b2fdaf6f
aidhayes <aid.hayes01@gmail.com>
2022-10-30 16:19:25 -0400

    Rework for use of rcg

--- gui.py
+++ gui.py
@@ -2,7 +2,7 @@ import PySimpleGUI as sg # python3 -m pip install pysimplegui
 from languages import lang_eng, text
 import bleu_score
 from nltk.tokenize import sent_tokenize
-import random
+import bert
 
 INPUT_BOX_SIZE = (50, 25)
 TITLE = 2
@@ -12,14 +12,12 @@ COMPARE = 3
 lang = "English"
 display = "Wikipedia Article Comparison Tool"
 
-def highlight_sim(element, text, colors, pairs):
+def highlight_sim(element, text, pairs):
     window[element].update("")
-    i = 0
     sentences = sent_tokenize(text)
     for sentence in sentences:
         if sentence in pairs:
-            window[element].update(sentence + " ", text_color_for_value="white", background_color_for_value = colors[i], append=True)
-            i = i + 1
+            window[element].update(sentence + " ", text_color_for_value="white", background_color_for_value = pairs[sentence][1], append=True)
         else:
             window[element].update(sentence + " ", text_color_for_value="black", append=True)
 
@@ -66,17 +64,9 @@ while True:
     if event == "-COMPARE-":
         ref = values["-TEXT 1-"]
         hyp = values["-TEXT 2-"]
-        pairs_ref, pairs_hyp = bleu_score.compare(ref, hyp)
-        window["-TEXT 1-"].update("")
-        colors = []
-        for i in range(len (pairs_ref)):
-            # https://stackoverflow.com/questions/13998901/generating-a-random-hex-color-in-python
-            highlight = "%06x" % random.randint(0, 0xFFFFFF)
-            highlight = "#" + highlight
-            colors.append(highlight)
-
-        highlight_sim("-TEXT 1-", ref, colors, pairs_ref)
-        highlight_sim("-TEXT 2-", hyp, colors, pairs_hyp)
+        pairs_ref, pairs_hyp = bert.bert(ref, hyp)
+        highlight_sim("-TEXT 1-", ref, pairs_ref)
+        highlight_sim("-TEXT 2-", hyp, pairs_hyp)
 
 window.close()
 
