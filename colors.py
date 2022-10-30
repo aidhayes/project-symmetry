d2ee18a1e483364ad975f60e5dc1bed0e431f3f1
aidhayes <aid.hayes01@gmail.com>
2022-10-30 16:18:22 -0400

    Random color generator

---
+++ colors.py
@@ -0,0 +1,7 @@
+import random
+
+# https://stackoverflow.com/questions/13998901/generating-a-random-hex-color-in-python
+def gen_color():
+    highlight = "%06x" % random.randint(0, 0xFFFFFF)
+    highlight = "#" + highlight
+    return highlight
